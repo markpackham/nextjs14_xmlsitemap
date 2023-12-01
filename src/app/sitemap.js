3edb9a4ac237d538c4973e3e27ea2bedbb0d427c
mark <markpackham1@gmail.com>
2023-12-01 13:21:23 +0000

    add posted data from database
    https://youtu.be/w29phXIag-4?si=4wZkCDtoHNHmVQBU&t=337

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,6 +1,19 @@
 import { connectToDatabase } from "@/utils/connectMongo";
 
-export default function sitemap() {
+export default async function sitemap() {
+  const client = await connectToDatabase();
+  // Sitemap is my database' name
+  const db = client.db("sitemap");
+  // Grab everything posts collection
+  const data = await db.collection("posts").find({}).toArray();
+
+  const post = data.map((item) => ({
+    url: `${item.url}`,
+    lastModified: `${item.lastModified}`,
+    changeFrequency: `${item.changeFrequency}`,
+    priority: `${item.priority}`,
+  }));
+
   return [
     {
       url: "https://acme.com",
