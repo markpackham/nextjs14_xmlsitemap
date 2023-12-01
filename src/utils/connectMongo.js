ee96f79016e9a53e3111058f999c4bb883c932d8
mark <markpackham1@gmail.com>
2023-12-01 12:40:25 +0000

    connect to local database
    https://youtu.be/w29phXIag-4?si=qgCJHJaEfGRDRXTN&t=173

---
+++ src/utils/connectMongo.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+const MONGODB_URI = process.env.MONGODB_URI;
+let client = null;
+
+export async function connectToDatabase() {
+  if (client) return client;
+
+  if (!MONGODB_URI) console.log("MongoDB URI not found!");
+
+  try {
+    cleint = await MongoClient.connect(MONGODB_URI);
+    console.log("Connected to MongoDB!");
+    return client;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
