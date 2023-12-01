7b06a16f25db0ea54f5aefefc35227aa350ecc7e
mark <markpackham1@gmail.com>
2023-12-01 13:45:42 +0000

    update xmlsitemap every hour
    https://youtu.be/w29phXIag-4?si=7tzjBuPmI1uIKBJS&t=506

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,6 +3,9 @@ import { connectToDatabase } from "@/utils/connectMongo";
 // Sitemap location
 // http://localhost:3000/sitemap.xml
 
+// Revalidate the page so we get the sitemap updated ever hour
+export const revalidate = 3600;
+
 export default async function sitemap() {
   const client = await connectToDatabase();
   // Sitemap is my database' name
